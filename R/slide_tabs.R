85f043ee6fbf88665add2900528a23b6b4e0abaf
Louisa Smith <louisahsmith@gmail.com>
2020-07-11 16:34:43 -0400

    trying to deal with videos

--- R/slide_tabs.R
+++ R/slide_tabs.R
@@ -10,19 +10,22 @@ slide_tabs <- function(slide_df, slide_url) {
     select_flag <- ifelse(selected, "true", "false")
     active_flag <- ifelse(selected, " active", "")
     out <- glue::glue('<li class="nav-item">\n',
-                      '<a class="nav-link{active_flag}" id="{slugify(name)}-tab" data-toggle="tab" ',
-                      'href="#{slugify(name)}" role="tab" ', 
+                      '<a class="nav-link{active_flag}" id="{slugify(name)}-tab" href="#{slugify(name)}" role="tab" ', 
                       'aria-controls="{slugify(name)}" aria-selected="{select_flag}">',
                       '{name}</a>\n',
                       '</li>')
     return(out)
   }
   
-  tab_pane <- function(name, slide, active, url) {
+  tab_pane <- function(name, slide, active, url, youtube) {
     select_flag <- ifelse(active, " show active", "")
     out <- glue::glue('<div class="tab-pane fade{select_flag}" id="{slugify(name)}" ',
                       'role="tabpanel" aria-labelledby="{slugify(name)}-tab">\n',
-                      '<div class="embed-responsive embed-responsive-16by10">\n',
+                      # '<div class="embed-responsive embed-responsive-16by9">\n',
+                      # '<iframe class="embed-responsive-item" src="{youtube}"',
+                      # 'frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"',
+                      # 'allowfullscreen></iframe>\n</div>\n',
+                      '<div class="embed-responsive embed-responsive-16by9">\n',
                       '<iframe class="embed-responsive-item" src="{url}#{slide}"></iframe>\n',
                       '</div>\n</div>')
     return(out)
@@ -31,7 +34,7 @@ slide_tabs <- function(slide_df, slide_url) {
   sections <- dplyr::mutate(
     slide_df,
     li = purrr::pmap_chr(list(name, active), nav_li),
-    pane = purrr::pmap_chr(list(name, slide, active, slide_url), tab_pane)
+    pane = purrr::pmap_chr(list(name, slide, active, slide_url, youtube), tab_pane)
   )
   
   tabset <- paste('<ul class="nav nav-tabs" id="slide-tabs" role="tablist">',
